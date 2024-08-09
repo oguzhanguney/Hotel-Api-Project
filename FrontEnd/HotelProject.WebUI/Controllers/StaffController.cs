94ebdf191a4a27dbeff73bf699eb6812fa3bbe38
oguzhanguney <oguzhan.guney098@gmail.com>
2024-08-09 18:21:17 +0300

    Add project files.

---
+++ FrontEnd/HotelProject.WebUI/Controllers/StaffController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class StaffController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
